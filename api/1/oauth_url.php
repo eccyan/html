d98ce04b8efe72643c0b8cb182fd39daa4db1dae
eccyan <g00.eccyan@gmail.com>
2011-02-04 23:36:48 +0900

    api for create oauth url

---
+++ api/1/oauth_url.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * URLパラメータを JSON 形式で出力する
+ */
+require_once("/var/www/html/path.php");
+require_once("configs/common.php");
+
+require_once('oauth/OAuth.php');
+
+$config = new ApplicationConfig();
+
+// get @Anywhere login value
+$anywhereId = explode(':', @$_COOKIE["twitter_anywhere_identity"]);
+$anywhere->id        = @$anywhereId[0];
+$anywhere->signature = @$anywhereId[1];
+// for validate anywhere signature
+$signature = @sha1(@$anywhere->id.$config->secret);
+
+$connect = memcache_connect('127.0.0.1', 11211);
+$cache = null;
+if ( !empty($anywhere->id)  && strcmp($anywhere->signature, $signature) == 0 ) {
+    $cache = memcache_get($connect, "{$anywhere->id}:{$anywhere->signature}");
+}
+
+$url = null;
+if ( !empty($cache) ) {
+    $decoded = json_decode($cache);
+
+    $endpoint = null;
+    $parameters = OAuthUtil::parse_parameters($_SERVER['QUERY_STRING']);
+    foreach ($parameters as $key => $parameter) {
+	if ( strcmp($key, 'ep') == 0 ) {
+	    $endpoint = $parameter;
+	}
+    }
+	    
+    $consumer        = new OAuthConsumer($config->key, $config->secret);
+    $signatureMethod = new OAuthSignatureMethod_HMAC_SHA1();
+    $token           = new OAuthToken($decoded->key, $decoded->secret);
+
+    $req = OAuthRequest::from_consumer_and_token($consumer, $token, NULL, $endpoint, $params);
+    $req->sign_request($signatureMethod, $consumer, $token);
+    $url = $req->to_url();
+}
+
+$responce = json_encode($url);
+
+header('Content-type: text/javascript; charset=utf-8');
+echo ($responce);
+?>
